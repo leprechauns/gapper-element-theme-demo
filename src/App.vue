52201e4804e07ad509fa2988437db65bbe0b4071
faithkobe <kobeluhaonan@163.com>
2018-05-25 16:12:27 +0800

    试一下提交还是不是两个人

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <layout></layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Layout from './components/LayOut.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'gapper-element-theme/lib/index.css'
+import './assets/normal.css'
 
 Vue.config.productionTip = false
 
